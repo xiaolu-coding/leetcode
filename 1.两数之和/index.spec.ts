782f1a9cda66434da0cbd10da29303a2cd80ec7a
xiaolu-coding <454819375@qq.com>
2022-05-16 16:02:22 +0800

    chore: 1.两数之和

--- 1.两数之和/index.spec.ts
+++ 1.两数之和/index.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from 'vitest';
+import { twoSum } from './index';
 
 test('准备测试环境', () => {
   expect(1).toBe(1);
+})
+
+test('测试twoSum', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
 })
