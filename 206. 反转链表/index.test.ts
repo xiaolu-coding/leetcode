9f5ab67c8485ec13d87fb00f3302130daeef91ec
xiaolu-coding <454819375@qq.com>
2022-05-20 08:48:05 +0800

    chore: 206.反转链表

---
+++ 206. 反转链表/index.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest"
+import { reverseList, ListNode } from "./index"
+
+describe("剑指 Offer II 022. 链表中环的入口节点", () => {
+  it("测试", () => {
+    let head: ListNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))
+    let result: ListNode = new ListNode(5, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(1)))))
+
+    expect(reverseList(head)).toEqual(result)
+  })
+})
---
+++ 206. 反转链表/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+export class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+export function reverseList(head: ListNode | null): ListNode | null {
+  if (head === null || head.next === null) {
+    return head
+  }
+
+  let last = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return last
+}
